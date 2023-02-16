18b98c1caa68e9fb289ea98b12759bc6a96380b0
unknown <benyapa.karmniyanont@gamil.com>
2023-02-16 11:00:55 +0700

    eidt 16-2-23

--- src/AppStart.js
+++ src/AppStart.js
@@ -4,53 +4,92 @@ import Todo from "./Todo.js"
 import React from "react";
 
 const initTasks = [
-  { id: 'todo-0', content: "Do Homework", done:false },
-  { id: 'todo-1', content: "Write a report", done:true },
-  { id: 'todo-2', content: "Hangout with friends", done:true },
-  { id: 'todo-3', content: "New Task", done:false },
+  { id: 'todo-0', content: "Do Homework", done: false },
+  { id: 'todo-1', content: "Write a report", done: true },
+  { id: 'todo-2', content: "Hangout with friends", done: true },
+  { id: 'todo-3', content: "New Task", done: false },
 ];
 // function App() {
 
-  class App extends React.Component {
+class App extends React.Component {
 
-    constructor(props){
-      super(props);
+  constructor(props) {
+    super(props);
 
-      this.state = {taskList : initTasks}
-    }
+    this.state = { taskList: initTasks, filter: "all" }
+    this.onFilter = this.onFilter.bind(this);
+  }
 
-    render(){
-      return (
-        <div className="container">
-      {/* make this h1 to be Title component */}
-      <h1>Todo List</h1>
+  onFilter = e => {
+    e.preventDefault();
+    console.log(e.target, e.target.value);
+    this.setState({ filter: e.target.value });
+  }
 
-      {/* make an AddTodo component  */}
-      {/* AddTodo start here */}
-      <AppTodo/>
-      {/* AddTodo end here */}
-
-      <span>
-      <label htmlFor="filter">Filter</label>
-      <select name="filter">
-        <option value="all">...</option>
-        <option value="done">Done</option>
-        <option value="active">Not Done Yet</option>
-      </select>
-      </span>
-
-      {/* make a Todos to display tasks from the given initTasks object */}
-
-      {/* <Todo/> */}
-      
-      {/* Todos end here */}
-      {/* <Todo taskList={initTasks} filter="all"/> */}
-      <Todo taskList={this.state.taskList} filter="all"/>
-
-    </div>
-      );
-    }
   
+  onCheckBox = id => {
+    
+    console.log("Appstart Checkbox",id);
+
+    let updateTask = this.state.taskList.map(
+      e => {
+        if(e.id === id){
+          e.done = !e.done;
+          return e;
+        }
+        return e;
+      }
+    );
+    this.setState({taskList: updateTask});
+    
+  }
+
+
+  onDelete = e => {
+    e.preventDefault();
+    console.log("delete");
+
+    // this.setState({ filter: e.target.value });
+  }
+
+  // onFilter = function(e){
+  //   e.preventDefault();
+  //   console.log(e.target,e.target.value);
+  //   this.setState( {filter: e.target.value});
+  // }
+
+  render() {
+    return (
+      <div className="container">
+        {/* make this h1 to be Title component */}
+        <h1>Todo List</h1>
+
+        {/* make an AddTodo component  */}
+        {/* AddTodo start here */}
+        <AppTodo />
+        {/* AddTodo end here */}
+
+        <span>
+          <label htmlFor="filter">Filter</label>
+          <select name="filter" onChange={this.onFilter}>
+            <option value="all">...</option>
+            <option value="done">Done</option>
+            <option value="active">Not Done Yet</option>
+          </select>
+        </span>
+
+        {/* make a Todos to display tasks from the given initTasks object */}
+
+        {/* <Todo/> */}
+
+        {/* Todos end here */}
+        {/* <Todo taskList={initTasks} filter="all"/> */}
+        <Todo taskList={this.state.taskList} filter={this.state.filter} onCheckBoxChange={this.onCheckBox} />
+
+      </div>
+    );
+  }
+
   // return (
   //   <div className="container">
   //     {/* make this h1 to be Title component */}
@@ -73,10 +112,10 @@ const initTasks = [
   //     {/* make a Todos to display tasks from the given initTasks object */}
 
   //     {/* <Todo/> */}
-      
+
   //     {/* Todos end here */}
   //     {/* <Todo taskList={initTasks} filter="all"/> */}
-  //     <Todo taskList={this.state.tastList} filter="all"/>
+  //     <Todo taskList={this.state.taskList} filter="all"/>
 
   //   </div>
   // );
--- src/Todo.js
+++ src/Todo.js
@@ -1,46 +1,59 @@
 import React from "react"
-function Todo(props){
+function Todo(props) {
 
     const filter = props.filter;
+    const onCheckBox = props.onCheckBoxChange;
+
+    const onCheckBoxChange = (id,e) => {
+        e.preventDefault();
+        console.log("Todos checkbox change: ",id);
+        onCheckBox(id);
+    }
+
+    const onDelete = props.onCheckBoxChange;
+    const onDeleteCheck = (id) => {
+        console.log("onDeleteCheck: ",id);
+        onDelete(id);
+    }
     const taskList = props.taskList;
     const listItem = taskList
-    .filter( e => {
-        if(filter === "done") return e.done;
-        if(filter ==="active") return !e.done;
-        return true;
-    }
-    )
-    .map(e => {
-
-        return(
-
-            <li key={e.id} className="list-group-item">
-          <div className="list-item">
-            <input id={e.id} type="checkbox" defaultChecked={`${e.done}`} />
-            <label htmlFor={e.id}>{e.content}</label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
-
-            // <li>
-            // {e.id},{e.content},{`${e.done}`}
-            // </li>
-            
-
+        .filter(e => {
+            if (filter === "done") return e.done;
+            if (filter === "active") return !e.done;
+            return true;
+        }
         )
+        .map(e => {
 
-    });
+            return (
 
-    return(
+                <li key={e.id} className="list-group-item">
+                    <div className="list-item">
+                        <input id={e.id} type="checkbox" defaultChecked={e.done} onChange={(event)=>onCheckBoxChange(e.id,event)} />
+                        <label htmlFor={e.id}>{e.content}</label>
+                    </div>
+                    <div className="btn-group">
+                        <button type="button" className="btn btn__danger" onClick={()=>onDeleteCheck(e.id)}>
+                            Delete
+                        </button>
+                    </div>
+                </li>
+
+                // <li>
+                // {e.id},{e.content},{`${e.done}`}
+                // </li>
+
+
+            )
+
+        });
+
+    return (
         <>
-        <h2 id="list-heading">3 tasks remaining</h2>
-      <ul className="list-group">
-        {listItem}
-        </ul>
+            <h2 id="list-heading">3 tasks remaining</h2>
+            <ul className="list-group">
+                {listItem}
+            </ul>
         </>
     )
 }
