2aef73126403bccdef5db0a5a99655fe018a505a
unknown <benyapa.karmniyanont@gamil.com>
2023-02-16 09:21:01 +0700

    edit 16-2-23

--- src/AppStart.js
+++ src/AppStart.js
@@ -1,15 +1,27 @@
 import "./App.css";
 import AppTodo from "./AddToDo.js"
-function App() {
-  const initTasks = [
-    { id: 'todo-0', content: "Do Homework", done:false },
-    { id: 'todo-1', content: "Write a report", done:true },
-    { id: 'todo-2', content: "Hangout with friends", done:true },
-    { id: 'todo-3', content: "New Task", done:false },
-  ];
+import Todo from "./Todo.js"
+import React from "react";
 
-  return (
-    <div className="container">
+const initTasks = [
+  { id: 'todo-0', content: "Do Homework", done:false },
+  { id: 'todo-1', content: "Write a report", done:true },
+  { id: 'todo-2', content: "Hangout with friends", done:true },
+  { id: 'todo-3', content: "New Task", done:false },
+];
+// function App() {
+
+  class App extends React.Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {taskList : initTasks}
+    }
+
+    render(){
+      return (
+        <div className="container">
       {/* make this h1 to be Title component */}
       <h1>Todo List</h1>
 
@@ -28,45 +40,46 @@ function App() {
       </span>
 
       {/* make a Todos to display tasks from the given initTasks object */}
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul className="list-group">
-        <li className="list-group-item">
-          <div className="list-item">
-            <input id="todo-0" type="checkbox" defaultChecked={false} />
-            <label htmlFor="todo-0">Do Homework</label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="list-group-item">
-          <div className="list-item">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label htmlFor="todo-0">Write a report</label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
-        <li className="list-group-item">
-          <div className="list-item">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label htmlFor="todo-0">Hang out with friends</label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn btn__danger">
-              Delete
-            </button>
-          </div>
-        </li>
-      </ul>
+
+      {/* <Todo/> */}
+      
       {/* Todos end here */}
+      {/* <Todo taskList={initTasks} filter="all"/> */}
+      <Todo taskList={this.state.taskList} filter="all"/>
 
     </div>
-  );
+      );
+    }
+  
+  // return (
+  //   <div className="container">
+  //     {/* make this h1 to be Title component */}
+  //     <h1>Todo List</h1>
+
+  //     {/* make an AddTodo component  */}
+  //     {/* AddTodo start here */}
+  //     <AppTodo/>
+  //     {/* AddTodo end here */}
+
+  //     <span>
+  //     <label htmlFor="filter">Filter</label>
+  //     <select name="filter">
+  //       <option value="all">...</option>
+  //       <option value="done">Done</option>
+  //       <option value="active">Not Done Yet</option>
+  //     </select>
+  //     </span>
+
+  //     {/* make a Todos to display tasks from the given initTasks object */}
+
+  //     {/* <Todo/> */}
+      
+  //     {/* Todos end here */}
+  //     {/* <Todo taskList={initTasks} filter="all"/> */}
+  //     <Todo taskList={this.state.tastList} filter="all"/>
+
+  //   </div>
+  // );
 }
+
 export default App;
---
+++ src/Todo.js
@@ -0,0 +1,47 @@
+import React from "react"
+function Todo(props){
+
+    const filter = props.filter;
+    const taskList = props.taskList;
+    const listItem = taskList
+    .filter( e => {
+        if(filter === "done") return e.done;
+        if(filter ==="active") return !e.done;
+        return true;
+    }
+    )
+    .map(e => {
+
+        return(
+
+            <li key={e.id} className="list-group-item">
+          <div className="list-item">
+            <input id={e.id} type="checkbox" defaultChecked={`${e.done}`} />
+            <label htmlFor={e.id}>{e.content}</label>
+          </div>
+          <div className="btn-group">
+            <button type="button" className="btn btn__danger">
+              Delete
+            </button>
+          </div>
+        </li>
+
+            // <li>
+            // {e.id},{e.content},{`${e.done}`}
+            // </li>
+            
+
+        )
+
+    });
+
+    return(
+        <>
+        <h2 id="list-heading">3 tasks remaining</h2>
+      <ul className="list-group">
+        {listItem}
+        </ul>
+        </>
+    )
+}
+export default Todo;
